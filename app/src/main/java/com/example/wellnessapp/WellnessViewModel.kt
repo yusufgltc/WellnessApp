12b10c72129bb634beee38449cc69e39199bf9ab
yusufgltc <ygultac@hotmail.com>
2023-10-18 00:28:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/wellnessapp/WellnessViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.wellnessapp
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel: ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val task : List<WellnessTask> get() = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) = task.find { it ->
+        it.id == item.id
+    }?.let { task->
+        task.checked = checked
+    }
+}
+
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
